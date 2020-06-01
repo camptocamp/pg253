c7290dde028149941884ff7e73b858cee04a2595
Julien Acroute <julien.acroute@camptocamp.com>
2020-06-01 14:15:45 +0200

    Add test data for blocking stderr

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -13,13 +13,17 @@ INSERT INTO data (data) SELECT g.id::text || 'Some more data to fill the DB' FRO
 CREATE DATABASE application;
 \c application;
 
-CREATE TABLE data(
-  id   serial,
-  data text
-);
 
--- Generate a 5Mo DB
-INSERT INTO data (data) SELECT g.id::text || 'Some more data to fill the DB' FROM generate_series(1, (6000000 / 438) * 5) AS g (id);
+CREATE OR REPLACE FUNCTION create_some_tables(tables_count integer, row_count integer) RETURNS void AS $$
+BEGIN
+	FOR i IN 1..tables_count LOOP
+      EXECUTE 'CREATE TABLE data' || i || '(id serial, data text)';
+      EXECUTE 'INSERT INTO data' || i || ' (data) SELECT g.id::text || ''Some more data to fill the DB'' FROM generate_series(1, ' || row_count || ') AS g (id)';
+	END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT create_some_tables(1000, 10000);
 
 -- Data DB
 CREATE DATABASE data;
