67433fc1362c0225ffb1cda828deb4d370915987
Léo Depriester <leo.depriester@camptocamp.com>
2026-04-18 23:24:28 +0200

    refactor(utils): lint code

--- src/pg253/utils.py
+++ src/pg253/utils.py
@@ -1,7 +1,11 @@
+""" Module containing utility functions for common processing tasks. """
+
 def sizeof_fmt(num):
+    """ Returns a formatted size in bytes for a given raw value. """
+
     num = int(num)
     for unit in ['B', 'KB', 'MB', 'GB']:
         if abs(num) < 1024.0:
-            return "%3.1f%s" % (num, unit)
+            return f"{num:3.1f}{unit}"
         num /= 1024.0
-    return "%.1f%s" % (num, 'TB')
+    return "{num:.1f}TB"
